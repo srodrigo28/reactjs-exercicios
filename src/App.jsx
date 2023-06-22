40dc3bdb841922f60a6626b1072067ad6ca81084
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-06-22 17:42:00 -0300

    update-project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import  {Contador}  from './classe/contador'
+// import { Contador } from './classe/contador'
+import { AppMega5 } from './Views/AppMega5'
 
 export function App() {
   return (
-    <Contador />
+    <AppMega5 />
   )
 }
